7c894f415cfa0fcaa3a85ccd9df7c494f5c38ed4
Abhinavv15 <abhinavdhanesh15@gmail.com>
2025-01-17 12:29:22 +0530

    Completed Virtual Assistant API task

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ Requirements:
 1. Create a GET endpoint at "/assistant/greet".
 2. The endpoint should accept a "name" as a query parameter (e.g., /assistant/greet?name=John).
 3. The API should return a JSON response with:
-   a. A personalized greeting using the name provided.
-   b. A cheerful message based on the current day of the week.
+    a. A personalized greeting using the name provided.
+    b. A cheerful message based on the current day of the week.
 
 Example Responses:
 - For Monday:
@@ -32,6 +32,36 @@ Example Responses:
 
 Add the required logic below to complete the API.
 */
+
+const dayMessage=()=>{
+  const message=[
+    "Happy Monday! Start your week with energy!",
+    "Have a wonderful day!",
+    "Have a wonderful day!",
+    "Have a wonderful day!",
+    "It's Friday! The weekend is near!",
+    "Have a wonderful day!",
+    "Have a wonderful day!",
+  ]
+  const day=new Date().getDay()
+  return message[day]
+}
+
+app.get('/assistant/greet',(req,res)=>{
+  const name=req.query.name
+
+  if(!name){
+    return res.status(404).json({error:"Name is required"})
+  }
+
+  const welcomeMessage=`Hello, ${name}! Welcome to our assistant app!`
+  const message2=dayMessage()
+  
+  
+
+  res.status(200).json({welcomeMessage,message2})
+})
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
